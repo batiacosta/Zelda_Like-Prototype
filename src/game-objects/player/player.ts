2cc0fb64119169933b972d5f71ad1b7d55b0eb6e
David Acosta <david.acosta.ing@gmail.com>
2025-04-08 12:29:32 -0500

    GameObject component and ControlsComponent

--- src/game-objects/player/player.ts
+++ src/game-objects/player/player.ts
@@ -1,5 +1,6 @@
 import { PLAYER_ANIMATION_KEYS } from '../../common/assets';
 import { Position } from '../../common/types';
+import { ControlsComponent } from '../../components/game-object/controls-component';
 import { InputComponent } from '../../components/input/input-component';
 export type PlayerConfig = {
   scene: Phaser.Scene;
@@ -10,7 +11,7 @@ export type PlayerConfig = {
 };
 
 export class Player extends Phaser.Physics.Arcade.Sprite {
-  #controls: InputComponent;
+  #controlsComponent: ControlsComponent;
   constructor(config: PlayerConfig) {
     const { scene, position, assetKey, frame } = config;
     const { x, y } = position;
@@ -18,7 +19,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
 
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.#controls = config.controls;
+    this.#controlsComponent = new ControlsComponent(this, config.controls);
     this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 }); // -1 to repeat indefinitly
 
     config.scene.events.on(Phaser.Scenes.Events.UPDATE, this.update, this); // Listen the event emmited in scene Update and excecute this.update()
@@ -33,16 +34,17 @@ export class Player extends Phaser.Physics.Arcade.Sprite {
   }
 
   update(): void {
-    if (this.#controls.isUpDown) {
+    const controls = this.#controlsComponent.controls;
+    if (controls.isUpDown) {
       this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_UP, repeat: -1 }, true);
-    } else if (this.#controls.isDownDown) {
+    } else if (controls.isDownDown) {
       this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_DOWN, repeat: -1 }, true);
     }
 
-    if (this.#controls.isLeftDown) {
+    if (controls.isLeftDown) {
       this.setFlipX(true);
       this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
-    } else if (this.#controls.isRightDown) {
+    } else if (controls.isRightDown) {
       this.setFlipX(false);
       this.play({ key: PLAYER_ANIMATION_KEYS.IDLE_SIDE, repeat: -1 }, true);
     }
